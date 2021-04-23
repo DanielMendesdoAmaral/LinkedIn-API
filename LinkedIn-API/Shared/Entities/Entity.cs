c2cccd1b82f221128a4ccb3f48b074f33a401962
DanielMendesdoAmaral <daniel.amaral720@gmail.com>
2021-04-23 11:09:48 -0300

    Projetos criados

---
+++ LinkedIn-API/Shared/Entities/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Shared.Entities
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; private set; }
+        public DateTime DataCriacao { get; private set; }
+
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+            DataCriacao = DateTime.Now;
+        }
+    }
+}
